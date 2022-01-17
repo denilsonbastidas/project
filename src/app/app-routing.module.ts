bb62a5b7af85c1467acd7184cd5c755f61db79e7
denilsonbastidas <denilsonbastidas3@gmail.com>
2022-01-16 20:23:48 -0400

    slider and header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AllproyectosComponent } from './components/allproyectos/allproyectos.component'
 
-const routes: Routes = [
-  {path: 'Allprojects', component: AllproyectosComponent},
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
